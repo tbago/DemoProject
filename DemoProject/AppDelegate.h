ac7078ab8ba9fdeacad015616602cc20a400d3a0
anxs <anxs@htlhomedeiMac-21.local>
2015-05-08 11:32:25 +0800

    Initial Commit

---
+++ DemoProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoProject
+//
+//  Created by anxs on 15/5/8.
+//  Copyright (c) 2015年 tbago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
